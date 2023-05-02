a3548299eed163067148a907b00225436191a626
Laura Nguyen Trong <laurangt@yahoo.com>
2023-05-02 12:45:06 +0200

    interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   // selector: '[app-servers]',  // [] as an attribute
   selector: '.app-servers', // as  a class
-  template: '<app-server></app-server>',
+  // template: '<app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 
 export class ServersComponent {
+  allowNewServer = false
 
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+  }
+  
 }
