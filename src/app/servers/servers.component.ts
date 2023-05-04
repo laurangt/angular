c615b0f8836bac90839d1d77652bde2f51bc6ee2
Laura Nguyen Trong <laurangt@yahoo.com>
2023-05-04 09:42:54 +0200

    components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,11 +10,43 @@ import { Component } from '@angular/core';
 
 export class ServersComponent {
   allowNewServer = false
+  serverCreationStatus = 'No server was created!'
+  serverName = 'Testserver'
+  username=''
+  serverCreated=false
+  servers=['testserver', 'testserver2']
+  showParagraph = true;
+  nbrBtn=[]
 
   constructor(){
     setTimeout(() => {
       this.allowNewServer = true
     }, 2000);
   }
-  
+
+  onUsernameInput(event: Event){
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! ' + this.serverName
+    this.serverCreated=true
+    this.servers.push(this.serverName)
+  }
+
+  onUpdateServerName(event: Event){
+    console.log(event)
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  resetInput(){
+    this.username=''
+  }
+
+  toggleDisplay(){
+    this.showParagraph = !this.showParagraph
+    // this.nbrBtn.push(this.nbrBtn.length + 1)
+    this.nbrBtn.push(new Date())
+
+  }
 }
